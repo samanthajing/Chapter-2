9488adeb34ebfbb88bbfcedd9e7f7b3ddb398698
samanthajing <samanthajing6529@gmail.com>
2026-09-14 13:27:04 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,13 @@
+public class Date {
+
+	public static void main (String[] args) {
+		
+			String day = "Monday";
+			int date = 14;
+			String month = "September";
+			int year = 2026;
+			System.out.print("American format" + ": " + day + ", " + month + date + "," + year);
+			
+	}
+
+}
